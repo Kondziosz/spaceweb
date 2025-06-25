44177302a5d14c4018ea6c47eb5f7058c4f5dce9
Kondziosz <cielinskikonrad@gmail.com>
2025-06-25 23:58:22 +0200

    working mobile raw, pc in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './Navbar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div className='w-screen max-w-none h-screen bg-home touch-none flex flex-col '>
+        <Navbar />
+        <div className='gap-300 flex flex-1 lg:justify-center'>
+        <div className='p-300 lg:p-0 lg:py-1600 gap-100 lg:max-w-[1110px] lg:gap-600 flex flex-1 flex-col lg:flex-row justify-start items-center'>
+        <div className='flex-1 flex-col justify-between items-center gap-600 '>
+        <div className='max-w-[540px] flex flex-col items-center gap-300'>
+        <p className='tp-6 text-[#D0D6F9] mt-8 '>SO, YOU WANT TO TRAVEL TO</p>
+        <p className='tp-1'>SPACE</p>
+        <p className='tp-9 mx-6 text-center text-[#D0D6F9] '> Let’s face it; if you want to go to space, you might as well genuinely go to
+  outer space and not hover kind of on the edge of it. Well sit back, and relax
+  because we’ll give you a truly out of this world experience!</p>
+        </div>
+        </div>
+        <div className='flex-1 gap-600 max-w-[540px] flex justify-center items-center'>
+        <div className="w-[144px] lg:w-[272px] h-[144px] lg:h-[272px] bg-white rounded-full flex items-center justify-center">
+        <span className=' text-black tp-4 !tracking-[0px] !leading-[1]'>EXPLORE</span>
+        </div>
+        </div>
 
+         </div>
+        </div>
+        </div>
 
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+       spacing: {
+        /*  Figma token   px    rem  */
+        "25":  "0.125rem",  //  2  px
+        "50":  "0.25rem",   //  4  px
+        "100": "0.5rem",    //  8  px
+        "150": "0.75rem",   // 12  px
+        "200": "1rem",      // 16  px
+        "300": "1.5rem",    // 24  px
+        "400": "2rem",      // 32  px
+        "500": "2.5rem",    // 40  px
+        "600": "3rem",      // 48  px
+        "800": "4rem",      // 64  px
+        "1000": "5rem",     // 80  px
+        "1200": "6rem",     // 96  px
+        "1600": "8rem"      // 128 px
+      }
+    },
   },
   plugins: [],
 }
