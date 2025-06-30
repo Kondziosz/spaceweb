fee9423dcc1b99bc4d2aaf3801a21023373ebb9d
Kondziosz <cielinskikonrad@gmail.com>
2025-06-30 23:51:40 +0200

    working mobile raw, working pc raw

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,22 @@ function App() {
     <>
       <div className='w-screen max-w-none h-screen bg-home touch-none flex flex-col '>
         <Navbar />
-        <div className='gap-300 flex flex-1 lg:justify-center'>
-        <div className='p-300 lg:p-0 lg:py-1600 gap-100 lg:max-w-[1110px] lg:gap-600 flex flex-1 flex-col lg:flex-row justify-start items-center'>
-        <div className='flex-1 flex-col justify-between items-center gap-600 '>
-        <div className='max-w-[540px] flex flex-col items-center gap-300'>
-        <p className='tp-6 text-[#D0D6F9] mt-8 '>SO, YOU WANT TO TRAVEL TO</p>
-        <p className='tp-1'>SPACE</p>
-        <p className='tp-9 mx-6 text-center text-[#D0D6F9] '> Let’s face it; if you want to go to space, you might as well genuinely go to
+        <div className='gap-100 flex flex-1 p-300 lg:justify-center lg:p-0 lg:py-1600 lg:gap-100 '>
+        <div className='flex flex-col flex-1 gap-600 lg:max-w-[1110px] lg:flex-row '>
+        <div className='flex-1 flex flex-col justify-between lg:h-fit lg:flex-row lg:mt-auto '>
+        <div className='max-w-[540px] flex flex-col w-fit items-center gap-300 space-y-[1px] lg:flex-1 lg:items-start'>
+        <p className='tp-6 !leading-[1] !tracking-[0.15em] !font-["Barlow_Condensed"] text-[#D0D6F9] '>SO, YOU WANT TO TRAVEL TO</p>
+        <p className='tp-1 !leading-[1]'>SPACE</p>
+        <p className='tp-9 text-center lg:text-left text-[#D0D6F9]'> Let’s face it; if you want to go to space, you might as well genuinely go to
   outer space and not hover kind of on the edge of it. Well sit back, and relax
   because we’ll give you a truly out of this world experience!</p>
         </div>
-        </div>
-        <div className='flex-1 gap-600 max-w-[540px] flex justify-center items-center'>
+        <div className='flex-1 gap-300 max-w-[540px] flex justify-center items-center lg:justify-end lg:gap-300'>
         <div className="w-[144px] lg:w-[272px] h-[144px] lg:h-[272px] bg-white rounded-full flex items-center justify-center">
         <span className=' text-black tp-4 !tracking-[0px] !leading-[1]'>EXPLORE</span>
         </div>
         </div>
-
+        </div>
          </div>
         </div>
         </div>
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,11 +2,13 @@ function Navbar()
  {
 	return(
 <div className='flex justify-between lg:justify-normal items-center shrink-0 py-300 lg:py-0 lg:pt-500 '>
-				<div className='flex items-center pl-300 lg:pl-800  gap-800'>
+				<div className='flex flex-1 items-center pl-300 lg:pl-800  gap-800 '>
         <img src="/logo.svg" className='h-[40px] lg:h-[48px]'/>
 				<div className="min-w-[560px] h-[1px] opacity-25 bg-white hidden lg:block z-10"/>
 				</div>
-        <img src="/icon-hamburger.svg" className=' h-[21px] pr-300 gap-100 md:hidden' />
+				<div className="flex-1 h-full md:hidden flex justify-end pr-300 gap-100 items-center">
+        <img src="/icon-hamburger.svg" className=' h-[21px] ' />
+				</div>
 				<div className='hidden lg:flex justify-end gap-600 lg:px-800 min-w-[664px] w-full h-max bg-[rgba(255,255,255,0.05)] backdrop-blur-[80px] ml-auto'>
 				<div className='border-b-[3px] border-white gap-100 h-[96px] flex items-center'>
 					<div className="gap-100 w-auto flex flex-row"><p className="tp-8b !tracking-[2.7px]">00</p><p className="tp-8">HOME</p></div></div>
